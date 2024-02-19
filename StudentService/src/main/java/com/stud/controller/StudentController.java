35c80ac225590a9a0357236cdf56be672e2f9a4a
arjunoggu <mallikarjundev2@gmail.com>
2024-02-18 19:15:02 -0500

    added resilence4j

--- StudentService/src/main/java/com/stud/controller/StudentController.java
+++ StudentService/src/main/java/com/stud/controller/StudentController.java
@@ -2,9 +2,11 @@ package com.stud.controller;
 
 import com.stud.entity.Student;
 import com.stud.service.StudentService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.concurrent.CompletableFuture;
@@ -23,7 +25,13 @@ public class StudentController {
     }
 
     @GetMapping("/{id}")
+    @CircuitBreaker(name = "studentservice", fallbackMethod = "fallbackMethod")
     public CompletableFuture<String> getStudentWithDepartment(@PathVariable Long id){
         return CompletableFuture.supplyAsync(()->studentService.getStudentWithDepartment(id));
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public CompletableFuture<String> fallbackMethod(@PathVariable Long id, RuntimeException e){
+        return CompletableFuture.supplyAsync(()->"Service is down. Please try after some time.");
     }
 }
